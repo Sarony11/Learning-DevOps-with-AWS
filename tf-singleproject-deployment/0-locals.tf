1e08de620e469119b82feaf79dbfb288f3638a47
Saul Fernandez <sarony11@gmail.com>
2022-04-17 02:24:41 +0200

    variables:added config file with no secrets and used for local definitions

--- tf-singleproject-deployment/0-locals.tf
+++ tf-singleproject-deployment/0-locals.tf
@@ -1,8 +1,9 @@
-/* locals {
+locals {
     config = jsondecode(file("../config.json"))
-    project_name = local.config["name"]
-    project_repo = local.config["repository"]
-    infra_env = local.config["environment"][0][0]
-    environment = "${project_name}-${infra_env}"
-    //aws_profile = local.config["aws_profile"][0]
-} */
+    project_name = local.config["project"]["name"]
+    project_repo = local.config["project"]["repository"]
+    infra_env = local.config["project"]["environment"]
+    environment = "dev"
+    aws_profile = local.config["project"]["environment"][local.environment]["aws"]["profile"]
+    aws_region = local.config["project"]["environment"][local.environment]["aws"]["region"]
+}
