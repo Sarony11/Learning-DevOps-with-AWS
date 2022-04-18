ee5422acccd38afa12fce93cdd8552e11778dd99
Saul Fernandez <sarony11@gmail.com>
2022-04-18 03:25:02 +0200

    step1:completed step1

--- infra/step-1/1-backend.tf
+++ infra/step-1/1-backend.tf
@@ -1,14 +1,3 @@
-/* 
-README
-Before applying this backend, we need to have the state bucket already created. For this reason, we need to comment this file,
-create the bucket, uncomment it again and finally, apply.
-STEP BY STEP
-1. Comment terraform section
-2. terraform init && terraform apply --auto-approve
-3. Uncomment terraform section
-4. terraform init && terraform apply --auto-approve 
-*/
-
 terraform {
 
     backend "s3" {
