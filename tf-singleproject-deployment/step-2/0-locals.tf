96e4560f563819e7149cdad63e521638a1605560
Saul Fernandez <sarony11@gmail.com>
2022-04-20 19:28:19 +0200

    Cleaning the project folders

--- tf-singleproject-deployment/step-2/0-locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-    config = jsondecode(file("../../config.json"))
-    project_name = local.config["project"]["name"]
-    project_repo = local.config["project"]["repository"]
-    infra_env = "dev"
-    aws_profile = local.config["project"]["environment"][local.infra_env]["aws"]["profile"]
-    aws_region = local.config["project"]["environment"][local.infra_env]["aws"]["region"]
-    vpc_cidr = 
-}
